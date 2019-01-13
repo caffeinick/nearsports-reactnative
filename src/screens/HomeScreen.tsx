7df9599f3da0fe969071c9664dd8e0a9b220a0d9
caffeinick <nick@collab.ee>
2019-01-14 01:58:48 +0900

    update HomeScreen.tsx to show example

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const points = [
+  {
+    latitude: 37.497896,
+    longitude: 127.027631,
+  },
+  {
+    latitude: 37.500786,
+    longitude: 127.036918,
+  },
+];
 
 class HomeScreen extends Component {
+  getRegionForCoordinates(points) {
+    // points should be an array of { latitude: X, longitude: Y }
+    let minX, maxX, minY, maxY;
+
+    // init first point
+    (point => {
+      minX = point.latitude;
+      maxX = point.latitude;
+      minY = point.longitude;
+      maxY = point.longitude;
+    })(points[0]);
+
+    // calculate rect
+    points.map(point => {
+      minX = Math.min(minX, point.latitude);
+      maxX = Math.max(maxX, point.latitude);
+      minY = Math.min(minY, point.longitude);
+      maxY = Math.max(maxY, point.longitude);
+    });
+
+    const midX = (minX + maxX) / 2;
+    const midY = (minY + maxY) / 2;
+    const deltaX = (maxX - minX) * 1.6;
+    const deltaY = (maxY - minY) * 1.6;
+
+    return {
+      latitude: midX,
+      longitude: midY,
+      latitudeDelta: deltaX,
+      longitudeDelta: deltaY,
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-      </View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.container}
+        initialRegion={this.getRegionForCoordinates(points)}
+      />
     );
   }
 }
